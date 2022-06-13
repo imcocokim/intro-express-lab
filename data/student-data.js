25e5e345e76e84138a3600bb8159d04d68957c1d
imcoco <imcoco@gmail.com>
2022-06-13 18:02:39 -0400

    add data in student-data.js file

--- data/student-data.js
+++ data/student-data.js
@@ -1,3 +1,11 @@
 const students = [
-
+  {name: "Lisa", enrolled: false, studentId: 12345},
+  {name: "Nicole", enrolled: true, studentId: 12346},
+  {name: "Paul", enrolled: false, studentId: 12347},
+  {name: "Paolo", enrolled: true, studentId: 12348},
+  {name: "Hector", enrolled: true, studentId: 12349},
 ]
+
+export {
+  students
+}

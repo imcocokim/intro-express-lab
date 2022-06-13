a73bb94d9b1734cdb40b245b69514cbf3eef2c98
imcoco <imcoco@gmail.com>
2022-06-13 18:15:30 -0400

    add views/student file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // import modules
 
 import express from 'express'
-
+import { students } from './data/student-data.js'
 // Create Express app
 
 const app = express()
@@ -22,6 +22,13 @@ app.get('/', function(req, res) {
 app.get('/home', function(req, res) {
   res.render('home')
 })
+
+app.get('/students', function (req, res) {
+  res.render('students/index', {
+    students : students
+  })
+})
+
 // Tell the app to listen on port 3000
 
 app.listen(3000, function() {
